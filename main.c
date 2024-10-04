08801b4c934723f9fdfd3d1c89fb30314002db2e
Maybach7 <d.s.samusev@gmail.com>
2024-10-04 21:10:00 +0300

    Add echo_command func in main.c

--- main.c
+++ main.c
@@ -3,8 +3,26 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdbool.h>
 
 static const char HISTORY_FILE[] = "history.txt";
+
+void echo_command(char* command) {
+	while(*command == ' ') {
+		command++;
+	}
+
+	bool flag = true;
+	char* token = strtok(command, "\\n");
+	while(token != NULL) {
+		flag = false;
+		printf("%s\n", token);
+		token = strtok(NULL, "\\n");
+	}
+	if(flag) {
+		printf("\n");
+	}
+}
 
 int main(){
 	char* command;
@@ -22,7 +40,11 @@ int main(){
 		else {
 			add_history(command);
 		}
-		printf("%s\n", command);
+
+		if(strncmp(command, "echo", 4)==0) {
+			echo_command(command + 5);
+		}
+		else printf("%s\n", command);
 		free(command);
 	}
 	write_history(HISTORY_FILE);
